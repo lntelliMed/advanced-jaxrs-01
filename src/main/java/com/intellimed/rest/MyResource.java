dbe3826045c5b5039ef86b7e3a663d9c9280f31b
Sam Alsmadi <sam.alsmadi@gmail.com>
2017-06-17 10:34:09 -0400

    Inject query and path params to member variables

--- src/main/java/com/intellimed/rest/MyResource.java
+++ src/main/java/com/intellimed/rest/MyResource.java
@@ -3,18 +3,24 @@ package com.intellimed.rest;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("test")
-@Singleton
+@Path("{pathParam}/test")
+//@Singleton
 public class MyResource {
 
+	@PathParam("pathParam") private String pathParam;
+	@QueryParam("queryParam") private String queryParam;
+	
 	private int count; 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String testMethod(){
 		++count;
-		return "It works! This method was called " + count + " times";
+		return "It works! This method was called " + count
+				+ " times. The path param is " + pathParam + ", and the query param is " + queryParam;
 	}
 }
